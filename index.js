ab222154f16f3916edaaaf7aa903b8424676d984
surajjagtap9836 <surajjagtap9836@gmail.com>
2025-03-25 21:54:48 +0530

    learn get method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express=require("express")
+const express=require("express");
+const req = require("express/lib/request");
 const app =express();
 
 //console.dir(app);
@@ -8,3 +9,25 @@ let port=5204;
 app.listen(port , ()=>{
     console.log(`app is listening on port..${port}`)
 })
+
+// app.use((req,res)=>{
+//  console.log("request received");
+//  res.send("sending A response...")
+// })
+
+app.get("/",(req,res)=>{
+    res.send("<h1>This is Home Page...</h1>")
+
+})
+
+// app.get("*",(req,res)=>{
+//  res.send("The Page Cannot found please check path...")
+// })
+
+app.get("/:username/:id",(req,res)=>{
+ let {username,id}=req.params;
+ res.send(`<h1>Welcome to the page &nbsp;&nbsp;<u><i> @${username}..!</i></u><br> id:${id}</h1>`);
+ 
+})
+
+
